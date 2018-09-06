0e7410441caf0b3a4cc9aa9c4cf15cac77957d24
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-06 19:29:07 +0300

    Add files via upload

---
+++ Task1/Project/Shapes/Models/Classes/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shapes.Models.Classes
+{
+    public struct Point
+    {
+        public Point(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+        static double Distance(Point A, Point B)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
