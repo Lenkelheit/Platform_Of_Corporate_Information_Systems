ea26897df59fcd82003da686f3b30e54c7f144c9
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-06 22:19:27 +0300

    Update IFileManager.cs.

--- Task1/Project/Shapes/Models/Interfaces/IFileManager.cs
+++ Task1/Project/Shapes/Models/Interfaces/IFileManager.cs
@@ -1,8 +1,36 @@
+using System.Text;
+
 namespace Shapes.Models.Interfaces
 {
+    /// <summary>
+    /// This interface provides methods for writing to and reading from common file.
+    /// </summary>
     public interface IFileManager
     {
-        void ReadFromFile(System.IO.Stream fileStream);
-        void WtiteToFile(System.IO.Stream fileStream);
+        /// <summary>
+        /// Represents name of file.
+        /// </summary>
+        StringBuilder NameOfFile { get; }
+        // METHODS
+        /// <summary>
+        /// Writes some information to file.
+        /// </summary>
+        /// <param name="fileStream">
+        /// Stream for access to file.
+        /// </param>
+        /// <returns>
+        /// The value if writing to file was successfull.
+        /// </returns>
+        bool WtiteToFile(System.IO.Stream fileStream);
+        /// <summary>
+        /// Reads some information from file.
+        /// </summary>
+        /// <param name="fileStream">
+        /// Stream for access to file.
+        /// </param>
+        /// <returns>
+        /// The value if reading from file was successfull.
+        /// </returns>
+        bool ReadFromFile(System.IO.Stream fileStream);
     }
 }
