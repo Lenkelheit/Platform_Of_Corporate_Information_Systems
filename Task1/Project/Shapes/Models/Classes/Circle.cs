a47671f3a22b19d020ddbd5aae840e4411e718d9
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-06 19:35:18 +0300

    Add files via upload

---
+++ Task1/Project/Shapes/Models/Classes/Circle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Models.Classes
+{
+    public class Circle : ShapeBase
+    {
+        public override Point Center
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public override double Radius
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double GetPerimeter
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double GetSquare
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void ReadFromFile(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WtiteToFile(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Point GetMiddlePoint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task1/Project/Shapes/Models/Classes/Square.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Models.Classes
+{
+    public class Square : ShapeBase
+    {
+        public Point TopLeftPoint
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Point BottomRightPoint
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double GetPerimeter
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double GetSquare
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void ReadFromFile(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WtiteToFile(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Point GetMiddlePoint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
