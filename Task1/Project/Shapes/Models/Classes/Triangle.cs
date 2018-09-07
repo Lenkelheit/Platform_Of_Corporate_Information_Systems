cfe689ceb0a665feef585c3755f00c6a51eabbff
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-07 17:48:16 +0300

    Update Triangle.cs

--- Task1/Project/Shapes/Models/Classes/Triangle.cs
+++ Task1/Project/Shapes/Models/Classes/Triangle.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shapes.Models.Classes
 {
-    class Triangle
+    public class Triangle : ShapeBase
     {
+        public override double GetPerimeter
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override double GetSquare
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void ReadFromFile(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WtiteToFile(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Point GetMiddlePoint()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
