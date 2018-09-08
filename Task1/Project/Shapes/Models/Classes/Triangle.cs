7140f379b95b72888e9d23e7bbbb84cae464be7a
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-08 08:00:14 +0300

    Update Triangle.cs

--- Task1/Project/Shapes/Models/Classes/Triangle.cs
+++ Task1/Project/Shapes/Models/Classes/Triangle.cs
@@ -25,12 +25,12 @@ namespace Shapes.Models.Classes
             }
         }
 
-        public override void ReadFromFile(Stream fileStream)
+        public override void ReadFromFile(StreamReader readStream)
         {
             throw new NotImplementedException();
         }
 
-        public override void WtiteToFile(Stream fileStream)
+        public override void WtiteToFile(StreamWriter writeStream)
         {
             throw new NotImplementedException();
         }
