5363d5ae287b0450e469f0934a17ca8114eb3611
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-09-08 16:32:06 +0300

    Update Program.cs

--- Task1/Project/Task1/Program.cs
+++ Task1/Project/Task1/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Shapes;
+using Shapes.Models.Interfaces;
+using Shapes.Models.Enums;
+using System.IO;
 
 namespace Task1
 {
@@ -11,6 +14,31 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            List<IShape> shapes = new List<IShape>();
+
+            using (StreamWriter sw = new StreamWriter("треба файл!"))
+            {
+                IEnumerable<IShape> newShapes = from i in shapes
+                                                orderby i.GetSquare
+                                                select i;
+                foreach (List<IShape> shape in newShapes)
+                {
+                    sw.WriteLine(shape);
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter("треба файл!"))
+            {
+                IEnumerable<IShape> newColections = from i in shapes
+                                                    where i.GetQuarter == CoordinateQuarters.Third
+                                                    orderby i.GetPerimeter descending
+                                                    select i;
+                foreach (List<IShape> j in newColections)
+                {
+                    sw.WriteLine(j);
+                }
+            }
         }
     }
 }
+
