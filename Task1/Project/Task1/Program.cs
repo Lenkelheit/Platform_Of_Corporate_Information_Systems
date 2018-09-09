4b44cda999c7945b3ca1ded724093a8f4557db67
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-09-09 21:51:18 +0300

    Update Program.cs

--- Task1/Project/Task1/Program.cs
+++ Task1/Project/Task1/Program.cs
@@ -1,1 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shapes.Models.Classes;
+using Shapes.Models.Enums;
+using Shapes.Models.Interfaces;
+using Shapes;
+using System.IO;
 
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ShapeBase> shapes = new List<ShapeBase>();
+
+            // Read the data in the List collection
+
+            // Sort the collection ascending by the square and write the result into file1
+            IEnumerable<ShapeBase> newShapes = from i in shapes
+                                               orderby i.GetSquare
+                                               select i;
+            WriteToFile<IFileManager>(newShapes, "file1");
+
+            // Find shapes that lie in the third quarter of the coordinate plane and write them in a separate collection
+            // Sort that collection decending by the perimeters and write the result into file2
+            IEnumerable<ShapeBase> newColections = from i in shapes
+                                                   where i.GetQuarter == CoordinateQuarters.Third
+                                                   orderby i.GetPerimeter descending
+                                                   select i;
+            WriteToFile<IFileManager>(newColections, "file2");
+        }
+
+        public static void WriteToFile<IFileManager>(IEnumerable<IFileManager> collection, string fileName)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(fileName))
+            {
+                foreach (IFileManager item in collection)
+                {
+                    streamWriter.WriteLine(item);
+                }
+            }
+        }
+    }
+}
