670e29c0ecb1c9c0ea67fda54b5405a70e695e8d
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-09 11:15:12 +0300

    Update ShapeBase.cs

--- Task1/Project/Shapes/Models/Classes/ShapeBase.cs
+++ Task1/Project/Shapes/Models/Classes/ShapeBase.cs
@@ -43,12 +43,39 @@ namespace Shapes.Models.Classes
         /// </returns>
         protected abstract Point GetMiddlePoint();
         /// <summary>
-        /// When overridden in a derived class, reads information from file.
+        /// When overridden in a derived class, interprets string as numeric data.
+        /// </summary>
+        /// <param name="line">
+        /// The string data.
+        /// </param>
+        protected abstract void Interpret(string line);       
+        /// <summary>
+        /// Reads some information about circle from file.
         /// </summary>
         /// <param name="readStream">
-        /// The file stream.
+        /// Stream only for reading from file.
         /// </param>
-        public abstract void ReadFromFile(System.IO.StreamReader readStream);
+        /// <exception cref="System.Exception">
+        /// Thrown when the first word in line from file isn`t recognized.
+        /// </exception>
+        public void ReadFromFile(System.IO.StreamReader readStream)
+        {
+            //In "name" will be stored information that class "Circle" will be read.
+            StringBuilder name = new StringBuilder("");
+            char letter = ' ';
+            while ((letter = (char)readStream.Read()) != ' ')
+            {
+                name.Append(letter);
+            }
+            if (name.ToString() == "Circle")
+            {
+                Interpret(readStream.ReadLine());
+            }
+            else
+            {
+                throw new System.Exception("The data isn`t recognized.");
+            }
+        }
         /// <summary>
         /// When overridden in a derived class, writes information to file.
         /// </summary>
@@ -56,5 +83,49 @@ namespace Shapes.Models.Classes
         /// The file stream.
         /// </param>
         public abstract void WriteToFile(System.IO.StreamWriter writeStream);
+        /// <summary>
+        /// Creates classes that inherit from "ShapeBase".
+        /// </summary>
+        /// <param name="readStream">
+        /// Stream only for reading from file.
+        /// </param>
+        /// <returns>
+        /// Instance of the corresponding class.
+        /// </returns>
+        /// <exception cref="System.Exception">
+        /// Thrown when the first word in line from file isn`t recognized.
+        /// </exception>
+        public static ShapeBase CreateInstance(System.IO.StreamReader readStream)
+        {
+            //In "name" will be stored information about what class must be created.
+            StringBuilder name = new StringBuilder("");
+            char letter = ' ';
+            while ((letter = (char)readStream.Read()) != ' ') 
+            {
+                name.Append(letter);
+            }
+            if (name.ToString() == "Circle")
+            {
+                Circle circle = new Circle();
+                circle.Interpret(readStream.ReadLine());
+                return circle;
+            }
+            else if (name.ToString() == "Square")
+            {
+                Square square = new Square();
+                square.Interpret(readStream.ReadLine());
+                return square;
+            }
+            else if (name.ToString() == "Triangle") 
+            {
+                Triangle triangle = new Triangle();
+                triangle.Interpret(readStream.ReadLine());
+                return triangle;
+            }
+            else
+            {
+                throw new System.Exception("The data isn`t recognized.");
+            }
+        }        
     }
 }
