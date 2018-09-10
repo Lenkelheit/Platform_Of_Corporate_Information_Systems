32f3903d82b2ff3ae98e15bc839edf2ddeca9c1d
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-10 21:09:28 +0300

    Update Program.cs

--- Task1/Project/Task1/Program.cs
+++ Task1/Project/Task1/Program.cs
@@ -10,24 +10,24 @@ namespace Task1
 {
     class Program
     {
+        const string WRITE_FILE_1 = "file1";
+        const string WRITE_FILE_2 = "file2";
+        
         static void Main(string[] args)
         {
             List<ShapeBase> shapes = new List<ShapeBase>();
 
-            string file1 = "";
-            string file2 = "";
-
             // Read the data in the List collection
 
             // Sort the collection ascending by the square and write the result into file1
-            WriteToFile(shapes.OrderBy(shape => shape.GetSquare), file1);
+            WriteToFile(shapes.OrderBy(shape => shape.GetSquare), WRITE_FILE_1);
 
             // Find shapes that lie in the third quarter of the coordinate plane and write them in a separate collection           
-            LinkedList<ShapeBase> shapeBases = 
+            LinkedList<ShapeBase> shapesThirdQuarter = 
                 new LinkedList<ShapeBase>(shapes.Where(shape => shape.GetQuarter == CoordinateQuarters.Third));
             
             // Sort that collection decending by the perimeters and write the result into file2
-            WriteToFile(shapeBases.OrderByDescending(shape => shape.GetPerimeter), file2);
+            WriteToFile(shapesThirdQuarter.OrderByDescending(shape => shape.GetPerimeter), WRITE_FILE_2);
         }
 
         public static void WriteToFile<T>(IEnumerable<T> collection, string fileName) where T : IFileManager
