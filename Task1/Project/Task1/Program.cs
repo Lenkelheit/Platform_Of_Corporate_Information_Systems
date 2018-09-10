543bc18d1f8ab0bc0ad2e265647dd0b6635b8cb5
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-09-10 20:43:44 +0300

    Update Program.cs

--- Task1/Project/Task1/Program.cs
+++ Task1/Project/Task1/Program.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Shapes.Models.Classes;
 using Shapes.Models.Enums;
 using Shapes.Models.Interfaces;
-using Shapes;
 using System.IO;
 
 namespace Task1
@@ -17,22 +14,25 @@ namespace Task1
         {
             List<ShapeBase> shapes = new List<ShapeBase>();
 
+            string file1 = "";
+            string file2 = "";
+
             // Read the data in the List collection
 
             // Sort the collection ascending by the square and write the result into file1
-            WriteToFile(shapes.OrderBy(shape => shape.GetSquare), "file1");
+            WriteToFile(shapes.OrderBy(shape => shape.GetSquare), file1);
 
             // Find shapes that lie in the third quarter of the coordinate plane and write them in a separate collection           
             LinkedList<ShapeBase> shapeBases = 
-            new LinkedList<ShapeBase>(shapes.Where(shape => shape.GetQuarter == CoordinateQuarters.Third));
+                new LinkedList<ShapeBase>(shapes.Where(shape => shape.GetQuarter == CoordinateQuarters.Third));
             
             // Sort that collection decending by the perimeters and write the result into file2
-            WriteToFile(shapeBases.OrderByDescending(shape=>shape.GetPerimeter), "file2");
+            WriteToFile(shapeBases.OrderByDescending(shape => shape.GetPerimeter), file2);
         }
 
         public static void WriteToFile<T>(IEnumerable<T> collection, string fileName) where T : IFileManager
         {
-            using (StreamWriter streamWriter = new StreamWriter(fileName))
+            using (StreamWriter streamWriter = new StreamWriter(fileName + ".txt"))
             {
                 foreach (T item in collection)
                 {
