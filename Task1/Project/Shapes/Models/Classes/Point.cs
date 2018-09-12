83ba78dbcb48164e6f5d7b951df437e3e0616fbf
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-12 21:21:58 +0300

    Update Point.cs

--- Task1/Project/Shapes/Models/Classes/Point.cs
+++ Task1/Project/Shapes/Models/Classes/Point.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Math;
 
 namespace Shapes.Models.Classes
