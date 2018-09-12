997b636a16815130cb43e6a7f228214b969c334f
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-12 21:26:24 +0300

    Update Triangle.cs

--- Task1/Project/Shapes/Models/Classes/Triangle.cs
+++ Task1/Project/Shapes/Models/Classes/Triangle.cs
@@ -7,6 +7,7 @@ namespace Shapes.Models.Classes
     /// </summary>
     public class Triangle : ShapeBase
     {
+        //CONST    
         const uint ARGUMENT_AMOUNT = 6;
         // FIELDS
         Point first;
