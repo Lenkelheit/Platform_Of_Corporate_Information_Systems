e676330151ea202604f00d3831d0d81a5b44e6b8
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-29 10:59:54 +0300

    Update Pentagon.cs

--- Task2/Project/Shapes/Models/Pentagon.cs
+++ Task2/Project/Shapes/Models/Pentagon.cs
@@ -1,10 +1,21 @@
-﻿using System.ComponentModel;
-
 namespace Shapes.Models
 {
     [System.Serializable]
-    public class Pentagon : ShapeBase, INotifyPropertyChanged
+    public class Pentagon : ShapeBase
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public System.Windows.Media.Color Color { get; set; }
+        public System.Windows.Media.Color StrokeColor { get; set; }
+        public double StrokeThickness { get; set; }
+        public double Opacity { get; set; }
+        public System.Windows.Point[] Points { get; set; }
+
+        public Pentagon()
+        {
+            throw new System.NotImplementedException();
+        }
+        public bool HitTest(System.Windows.Point p)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
