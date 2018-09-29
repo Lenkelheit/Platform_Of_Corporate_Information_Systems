bc9dadd562638889cff2e76bb88b373cc8c15ef7
Kizlo Taras <orleon.nilkford@gmail.com>
2018-09-29 10:59:08 +0300

    Update ShapeBase.cs

--- Task2/Project/Shapes/Models/ShapeBase.cs
+++ Task2/Project/Shapes/Models/ShapeBase.cs
@@ -1,8 +1,15 @@
-﻿namespace Shapes.Models
+namespace Shapes.Models
 {
     [System.Serializable]
-    public abstract class ShapeBase
+    public abstract class ShapeBase : System.ComponentModel.INotifyPropertyChanged
     {
         public string Name => this.GetType().Name;
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
