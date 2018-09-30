a5551b9fd6936120f736ce41719dddbbb369b498
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-30 18:07:49 +0300

    Update ShapeBase.cs

--- Task2/Project/Shapes/Models/ShapeBase.cs
+++ Task2/Project/Shapes/Models/ShapeBase.cs
@@ -1,15 +1,29 @@
 namespace Shapes.Models
 {
+    /// <summary>
+    /// Represents basic algorithms for the shape objects.
+    /// </summary>
     [System.Serializable]
     public abstract class ShapeBase : System.ComponentModel.INotifyPropertyChanged
     {
-        public string Name => this.GetType().Name;
-
+        // PROPERTIES
+        /// <summary>
+        /// The name of shape.
+        /// </summary>
+        public string Name { get; set; }
+        // EVENTS
+        /// <summary>
+        /// Notifies that some property is changed.
+        /// </summary>
         public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
-
+        // METHODS
+        /// <summary>
+        /// Notifies event <see cref="PropertyChanged"/> that some property is changed.
+        /// </summary>
+        /// <param name="e">Data for event <see cref="PropertyChanged"/>.</param>
         protected virtual void OnPropertyChanged(System.ComponentModel.PropertyChangedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            PropertyChanged?.Invoke(this, e);
         }
     }
 }
