771e8a66a001021b85408bb1e8f130c77479f5ce
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-10-01 01:00:03 +0300

    Update RemoveVertex.cs

--- Task2/Project/Shapes/Commands/Vertex/RemoveVertex.cs
+++ Task2/Project/Shapes/Commands/Vertex/RemoveVertex.cs
@@ -1,23 +1,45 @@
-﻿namespace Shapes.Commands.Vertex
+namespace Shapes.Commands.Vertex
 {
+    /// <summary>
+    /// Represents command of removing <see cref="Models.Vertex"/>.
+    /// </summary>
     public class RemoveVertex : Interfaces.ICommand
     {
-        public string Name
+        // FIELDS
+        private Models.Vertex vertex;
+        private System.Windows.Point prevLocation;
+        // PROPERTIES
+        /// <summary>
+        /// Command name.
+        /// </summary>
+        public string Name => "Vertex removed";
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="vertex">Current vertex.</param>
+        public RemoveVertex(Models.Vertex vertex)
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+            this.vertex = vertex;
         }
-
+        // METHODS
+        /// <summary>
+        /// Removes <see cref="Models.Vertex"/>.
+        /// </summary>
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            if (vertex.Location.X != 0 || vertex.Location.Y != 0) 
+            {
+                prevLocation = vertex.Location;
+                vertex.Location = new System.Windows.Point(0, 0);
+            }
         }
-
+        /// <summary>
+        /// Restores removed <see cref="Models.Vertex"/>.
+        /// </summary>
         public void UnExecute()
         {
-            throw new System.NotImplementedException();
+            vertex.Location = prevLocation;
         }
     }
 }
