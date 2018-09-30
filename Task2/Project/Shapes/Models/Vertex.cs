7d4e72918d30a06416f4dd012f0b16ac39ca35f0
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-30 19:03:29 +0300

    Update Vertex.cs

--- Task2/Project/Shapes/Models/Vertex.cs
+++ Task2/Project/Shapes/Models/Vertex.cs
@@ -1,8 +1,28 @@
 namespace Shapes.Models
 {
+    /// <summary>
+    /// Represents one of points for creating the shape objects.
+    /// </summary>
     [System.Serializable]
     public class Vertex : ShapeBase
     {
-        public System.Windows.Point Location { get; set; }
+        // FIELDS
+        private System.Windows.Point location;
+        // PROPERTIES
+        /// <summary>
+        /// Point position on the coordinate plane.
+        /// </summary>
+        public System.Windows.Point Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+                OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("Location"));
+            }
+        }
     }
 }
