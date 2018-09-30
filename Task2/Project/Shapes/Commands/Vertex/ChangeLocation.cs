67af1227cc7b63b8b72631cf6b2f753d28fb1179
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-09-30 23:28:30 +0300

    Update ChangeLocation.cs

--- Task2/Project/Shapes/Commands/Vertex/ChangeLocation.cs
+++ Task2/Project/Shapes/Commands/Vertex/ChangeLocation.cs
@@ -1,23 +1,45 @@
-﻿namespace Shapes.Commands.Vertex
+namespace Shapes.Commands.Vertex
 {
+    /// <summary>
+    /// Represents command for <see cref="Models.Vertex"/> that changes its location.
+    /// </summary>
     public class ChangeLocation : Interfaces.ICommand
     {
-        public string Name
+        // FIELDS
+        private Models.Vertex vertex;
+        private System.Windows.Point location;
+        private System.Windows.Point prevLocation;
+        // PROPERTIES
+        /// <summary>
+        /// Command name.
+        /// </summary>
+        public string Name => "Location changed";
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="vertex">Current vertex.</param>
+        /// <param name="location">New location.</param>
+        public ChangeLocation(Models.Vertex vertex, System.Windows.Point location)
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+            this.vertex = vertex;
+            this.location = location;
         }
-
+        // METHODS
+        /// <summary>
+        /// Changes <see cref="Models.Vertex"/>'s state.
+        /// </summary>
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            prevLocation = vertex.Location;
+            vertex.Location = location;
         }
-
+        /// <summary>
+        /// Returns <see cref="Models.Vertex"/> to its previous state.
+        /// </summary>
         public void UnExecute()
         {
-            throw new System.NotImplementedException();
+            vertex.Location = prevLocation;
         }
     }
 }
