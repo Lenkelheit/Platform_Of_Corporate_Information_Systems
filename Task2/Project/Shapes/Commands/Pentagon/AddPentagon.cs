3626839c387957ed7f972f05a1336565bf1281b5
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-10-01 12:27:03 +0300

    Update AddPentagon.cs

--- Task2/Project/Shapes/Commands/Pentagon/AddPentagon.cs
+++ Task2/Project/Shapes/Commands/Pentagon/AddPentagon.cs
@@ -1,20 +1,25 @@
-﻿namespace Shapes.Commands.Pentagon
+namespace Shapes.Commands.Pentagon
 {
+    /// <summary>
+    /// Represents command of adding <see cref="Models.Pentagon"/>.
+    /// </summary>
     public class AddPentagon : Interfaces.ICommand
     {
-        public string Name
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-
+        // PROPERTIES
+        /// <summary>
+        /// Command name.
+        /// </summary>
+        public string Name => "Pentagon added";
+        /// <summary>
+        /// Adds <see cref="Models.Pentagon"/>.
+        /// </summary>
         public void Execute()
         {
             throw new System.NotImplementedException();
         }
-
+        /// <summary>
+        /// Restores previous state without added <see cref="Models.Pentagon"/>.
+        /// </summary>
         public void UnExecute()
         {
             throw new System.NotImplementedException();
