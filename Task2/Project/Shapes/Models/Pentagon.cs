259247ce07a5c1de78604dc0c2754d158cd1677f
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-10-02 20:28:21 +0300

    Update pentagon.cs and PentagonTest.cs

--- Task2/Project/Shapes/Models/Pentagon.cs
+++ Task2/Project/Shapes/Models/Pentagon.cs
@@ -1,18 +1,143 @@
+using System.ComponentModel;
+
 namespace Shapes.Models
 {
+    /// <summary>
+    /// Represents class that models Pentagon
+    /// </summary>
     [System.Serializable]
     public class Pentagon : ShapeBase
     {
-        public System.Windows.Media.Color Color { get; set; }
-        public System.Windows.Media.Color StrokeColor { get; set; }
-        public double StrokeThickness { get; set; }
-        public double Opacity { get; set; }
-        public System.Windows.Point[] Points { get; set; }
+        // FIELDS
+        System.Windows.Media.Color color;
+        System.Windows.Media.Color strokeColor;
+        double strokeThickness;
+        double opacity;
+        System.Windows.Point[] points;
 
+        // PROPERTIES
+        /// <summary>
+        /// Property that enable to interract with color
+        /// </summary>
+        /// /// <retruns>Pentagon color</retruns>
+        public System.Windows.Media.Color Color
+        {
+            get
+            {
+                return color;
+            }
+            set
+            {
+                if (color != value)
+                {
+                    color = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("Color"));
+                }
+            }
+        }
+        /// <summary>
+        /// Property that enable to interract with stroke color
+        /// </summary>
+        /// /// <retruns>Pentagon stroke color</retruns>
+        public System.Windows.Media.Color StrokeColor
+        {
+            get
+            {
+                return strokeColor;
+            }
+            set
+            {
+                if (strokeColor != value)
+                {
+                    strokeColor = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("StrokeColor"));
+                }
+            }
+        }
+        /// <summary>
+        /// Property that enable to interract with stroke thickness
+        /// </summary>
+        /// /// <retruns>Pentagon stroke thickness</retruns>
+        public double StrokeThickness
+        {
+            get
+            {
+                return strokeThickness;
+            }
+            set
+            {
+                if (strokeThickness != value)
+                {
+                    strokeThickness = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("StrokeThickness"));
+                }
+            }
+        }
+        /// <summary>
+        /// Property that enable to interract with opacity
+        /// </summary>
+        /// /// <retruns>Pentagon opacity</retruns>
+        public double Opacity
+        {
+            get
+            {
+                return opacity;
+            }
+            set
+            {
+                if (opacity != value)
+                {
+                    opacity = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("Opacity"));
+                }
+            }
+        }
+        /// <summary>
+        /// Property that enable to interract with Pentagon edge points
+        /// </summary>
+        /// <retruns>Pentagon stroke color</retruns>
+        System.Windows.Point[] Points
+        {
+            get
+            {
+                return points;
+            }
+            set
+            {
+                if (points.Length == 5)
+                {
+                    if (points != value)
+                    {
+                        points = value;
+                        OnPropertyChanged(new PropertyChangedEventArgs("Points"));
+                    }
+                }
+                else
+                {
+                    throw new System.ArgumentException("Pentagon should have 5 edges");
+                }
+            }
+        }
+
+        //CONSTRUCTORS
+        /// <summary>
+        /// Default constructor without parameters
+        /// </summary>
         public Pentagon()
         {
-            throw new System.NotImplementedException();
+            color = new System.Windows.Media.Color();
+            strokeColor = new System.Windows.Media.Color();
+            strokeThickness = 0;
+            opacity = 0;
+            points = new System.Windows.Point[5];
         }
+
+        //METHODS
+        /// <summary>
+        /// Method that check if point is in the shape
+        /// </summary>
+        /// <param name="p">Target point</param>
+        /// <returns>Availability point in shape</returns>
         public bool HitTest(System.Windows.Point p)
         {
             throw new System.NotImplementedException();
