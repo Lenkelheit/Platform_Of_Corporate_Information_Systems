543913534008f3d1af7152a4e05b0f4a78093480
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-10-02 20:29:37 +0300

    Revert "Update pentagon.cs and PentagonTest.cs"
    This reverts commit 259247ce07a5c1de78604dc0c2754d158cd1677f.

--- Task2/Project/Test/PentagonTest.cs
+++ Task2/Project/Test/PentagonTest.cs
@@ -1,74 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Shapes.Models;
-using System.Xml.Serialization;
-using System.IO;
 
 namespace Test
 {
     [TestClass]
     public class PentagonTest
     {
-        Pentagon test = new Pentagon();
-        #region PropertiesTest
         [TestMethod]
-        public void ColorPropertyUnitTest()
+        public void TestMethod1()
         {
-            test.Color = System.Windows.Media.Color.FromRgb(100, 100, 100);
-            Assert.AreEqual(test.Color, System.Windows.Media.Color.FromRgb(100, 100, 100));
-        }
-        [TestMethod]
-        public void StrokeColorPropertyUnitTest()
-        {
-            test.StrokeColor = System.Windows.Media.Color.FromRgb(100, 100, 100);
-            Assert.AreEqual(test.StrokeColor, System.Windows.Media.Color.FromRgb(100, 100, 100));
-        }
-        [TestMethod]
-        public void StrokeThicknessPropertyUnitTest()
-        {
-            test.StrokeThickness = 10;
-            Assert.AreEqual(test.StrokeThickness, 10);
-        }
-        [TestMethod]
-        public void OpacityPropertyUnitTest()
-        {
-            test.Opacity = 10;
-            Assert.AreEqual(test.Opacity, 10);
-        } 
-        #endregion
-
-         [TestMethod]
-         void SerialiseUnitTest()
-        {
-            XmlSerializer xmlFormat = new XmlSerializer(typeof(Pentagon),
-               new Type[] { typeof(System.Windows.Media.Color), typeof(System.Windows.Point[]) });
-            Pentagon temp = new Pentagon();
-            string fileName = @"..\..\Serialization\PentagonData.xml";
-            temp.Points = test.Points;
-            temp.Color = test.Color;
-            temp.StrokeColor = test.StrokeColor;
-            temp.Opacity = test.Opacity;
-            temp.StrokeThickness = test.StrokeThickness;
-            using (Stream fStream = new FileStream(fileName,
-                FileMode.Create, FileAccess.Write, FileShare.None))
-            {
-                xmlFormat.Serialize(fStream, temp);
-            }
-            using (Stream fStream = new FileStream(fileName,
-               FileMode.Open, FileAccess.Read, FileShare.None))
-            {
-                test = (Pentagon)xmlFormat.Deserialize(fStream);
-            }
-            if (temp.Points == test.Points &&
-            temp.Color == test.Color &&
-            temp.StrokeColor == test.StrokeColor &&
-            temp.Opacity == test.Opacity &&
-            temp.StrokeThickness == test.StrokeThickness)
-            {
-                Assert.IsTrue(true);
-            }
-            
-
         }
     }
 }
