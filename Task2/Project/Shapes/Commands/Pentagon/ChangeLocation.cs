f6d781f06452b0f264f15473009a72322325e0ad
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-10-03 21:30:26 +0300

    Update ChangeLocation.cs

--- Task2/Project/Shapes/Commands/Pentagon/ChangeLocation.cs
+++ Task2/Project/Shapes/Commands/Pentagon/ChangeLocation.cs
@@ -1,23 +1,49 @@
-﻿namespace Shapes.Commands.Pentagon
+namespace Shapes.Commands.Pentagon
 {
+    /// <summary>
+    /// Represents the command <see cref = "Models.Pentagon" /> which changes its location.
+    /// </summary>
     public class ChangeLocation : Interfaces.ICommand
     {
-        public string Name
+        // FIELDS
+        private Models.Pentagon pentagon;
+        private System.Windows.Point point;
+        private System.Windows.Point[] prevLocation;
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="pentagon">Current pentagon.</param>
+        /// <param name="point">New points.</param>
+        public ChangeLocation(Models.Pentagon pentagon, System.Windows.Point point)
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+            this.pentagon = pentagon;
+            this.point = point;
         }
-
+        // PROPERTIES
+        /// <summary>
+        /// Command name.
+        /// </summary>
+        public string Name => "Location change";
+        // METHODS
+        /// <summary>
+        /// Changes <see cref="Models.Pentagon"/> location.
+        /// </summary>
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            prevLocation = pentagon.Points;
+            for(int i=0; i<5; ++i)
+            {
+                pentagon.Points[i].X += point.X;
+                pentagon.Points[i].Y += point.Y;
+            }
         }
-
+        /// <summary>
+        /// Returns <see cref="Models.Pentagon"/> to its previous location.
+        /// </summary>
         public void UnExecute()
         {
-            throw new System.NotImplementedException();
+            pentagon.Points = prevLocation;
         }
     }
 }
