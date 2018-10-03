8ffcc33d86bce39fce8e48c6eafb2bb2cf36d731
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-10-03 21:28:37 +0300

    Update ChangeColor.cs

--- Task2/Project/Shapes/Commands/Pentagon/ChangeColor.cs
+++ Task2/Project/Shapes/Commands/Pentagon/ChangeColor.cs
@@ -1,23 +1,45 @@
-﻿namespace Shapes.Commands.Pentagon
+namespace Shapes.Commands.Pentagon
 {
+    /// <summary>
+    /// Represents the command <see cref = "Models.Pentagon" /> which changes its color.
+    /// </summary>
     public class ChangeColor : Interfaces.ICommand
     {
-        public string Name
+        // FIELDS
+        private Models.Pentagon Pentagon;
+        private System.Windows.Media.Color Color;
+        private System.Windows.Media.Color PrevColor;
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="pentagon">Current pentagon.</param>
+        /// <param name="color">New color.</param>
+        public ChangeColor(Models.Pentagon pentagon, System.Windows.Media.Color color)
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+            this.Pentagon = pentagon;
+            this.Color = color;
         }
-
+        // PROPERTIES
+        /// <summary>
+        /// Command name.
+        /// </summary>
+        public string Name => "Color changed";
+        // METHODS
+        /// <summary>
+        /// Changes <see cref="Models.Pentagon"/> color.
+        /// </summary>
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            PrevColor = Pentagon.Color;
+            Pentagon.Color = Color;
         }
-
+        /// <summary>
+        /// Returns <see cref="Models.Pentagon"/> to its previous color.
+        /// </summary>
         public void UnExecute()
         {
-            throw new System.NotImplementedException();
+            Pentagon.Color = PrevColor;
         }
     }
 }
