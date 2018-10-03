0533f9729528cebe28a3e8de74ceee806c1706fd
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-10-03 21:33:03 +0300

    Update ChangeStrokeColor.cs

--- Task2/Project/Shapes/Commands/Pentagon/ChangeStrokeColor.cs
+++ Task2/Project/Shapes/Commands/Pentagon/ChangeStrokeColor.cs
@@ -1,23 +1,45 @@
-﻿namespace Shapes.Commands.Pentagon
+namespace Shapes.Commands.Pentagon
 {
+    /// <summary>
+    /// Represents the command <see cref = "Models.Pentagon" /> which changes its stroke color.
+    /// </summary>
     public class ChangeStrokeColor : Interfaces.ICommand
     {
-        public string Name
+        // FILEDS
+        private Models.Pentagon Pentagon;
+        private System.Windows.Media.Color StrokeColor;
+        private System.Windows.Media.Color PrevStrokeColor;
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="pentagon">Current pentagon.</param>
+        /// <param name="StrokeColor">New color.</param>
+        public ChangeStrokeColor(Models.Pentagon pentagon, System.Windows.Media.Color StrokeColor)
         {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
+            this.Pentagon = pentagon;
+            this.StrokeColor = StrokeColor;
         }
-
+        // PROPERTIES
+        /// <summary>
+        /// Command name.
+        /// </summary>
+        public string Name => "StrokeColor changed";
+        // METHODS
+        /// <summary>
+        /// Changes <see cref="Models.Pentagon"/> stroke color.
+        /// </summary>
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            PrevStrokeColor = Pentagon.StrokeColor;
+            Pentagon.StrokeColor = StrokeColor;
         }
-
+        /// <summary>
+        /// Returns <see cref="Models.Pentagon"/> to its previous stroke color.
+        /// </summary>
         public void UnExecute()
         {
-            throw new System.NotImplementedException();
+            Pentagon.StrokeColor = StrokeColor;
         }
     }
 }
