2d6d53317dda8ccad127f20fa900900e5c5d5478
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-10-03 21:07:26 +0300

    RemovePentagon update

--- Task2/Project/Shapes/Commands/Pentagon/RemovePentagon.cs
+++ Task2/Project/Shapes/Commands/Pentagon/RemovePentagon.cs
@@ -1,25 +1,51 @@
-﻿namespace Shapes.Commands.Pentagon
+﻿using Shapes.Models;
+
+namespace Shapes.Commands.Pentagon
 {
+    /// <summary>
+    /// Class that represents command for removing pentagon
+    /// </summary>
     public class RemovePentagon : Interfaces.ICommand
     {
         // FIELDS
+        Canvas baseCanvas;
+        Models.Pentagon target;
+        int positionInCanvas;
 
+        // PROPETRIES
+        /// <summary>
+        /// Property that allows to get command name
+        /// </summary>
         public string Name
         {
             get
             {
-                throw new System.NotImplementedException();
+                return "Pentagon removed";
             }
         }
 
+        // METHODS
+        /// <summary>
+        /// Method that execute command
+        /// </summary>
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            for (int i = 0; i < baseCanvas.Count; i++)
+            {
+                if (baseCanvas[i] == target)
+                {
+                    positionInCanvas = i;
+                    break;
+                }
+            }
+            baseCanvas.Remove(target);
         }
-
+        /// <summary>
+        /// Method that returns command execution
+        /// </summary>
         public void UnExecute()
         {
-            throw new System.NotImplementedException();
+            baseCanvas.Insert(positionInCanvas, target);
         }
     }
 }
