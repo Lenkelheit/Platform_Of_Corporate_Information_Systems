ce11d6fcc2b654077a9a4448979d6ff1b4b061d5
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-10-04 14:21:06 +0300

    Update Vertex.cs

--- Task2/Project/Shapes/Models/Vertex.cs
+++ Task2/Project/Shapes/Models/Vertex.cs
@@ -6,6 +6,8 @@ namespace Shapes.Models
     [System.Serializable]
     public class Vertex : ShapeBase
     {
+        // CONST
+        private const int RADIUS = 5;
         // FIELDS
         private System.Windows.Point location;
         // PROPERTIES
@@ -33,7 +35,32 @@ namespace Shapes.Models
         /// </summary>
         public Vertex()
         {
-            location = new System.Windows.Point(0, 0);
+            location = new System.Windows.Point();
+        }
+        // METHODS
+        /// <summary>
+        /// Calculates distance between two vertices.
+        /// </summary>
+        /// <param name="firstVertex">First vertex.</param>
+        /// <param name="secondVertex">Second vertex.</param>
+        /// <returns>
+        /// The distance between two vertices.
+        /// </returns>
+        public static double GetDistance(Vertex firstVertex, Vertex secondVertex)
+        {
+            return System.Math.Sqrt(System.Math.Pow(secondVertex.Location.X - firstVertex.Location.X, 2) +
+                System.Math.Pow(secondVertex.Location.Y - firstVertex.Location.Y, 2));
+        }
+        /// <summary>
+        /// Checks if current point is in vertex.
+        /// </summary>
+        /// <param name="point">Current point.</param>
+        /// <returns>
+        /// True if current point is in vertex, else - false.
+        /// </returns>
+        public override bool HitTest(System.Windows.Point point)
+        {
+            return GetDistance(this, new Vertex { Location = point }) <= RADIUS;
         }
     }
 }
