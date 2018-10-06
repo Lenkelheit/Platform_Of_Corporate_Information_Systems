8f1b47db98ef54f911d83c3b9de71588f97f9802
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-10-06 17:39:13 +0300

    Update AddPentagon.cs

--- Task2/Project/Shapes/Commands/Pentagon/AddPentagon.cs
+++ Task2/Project/Shapes/Commands/Pentagon/AddPentagon.cs
@@ -93,7 +93,7 @@ namespace Shapes.Commands.Pentagon
                 Points = arrPoints
             };
             canvas.RemoveAll(shape => shape is Models.Vertex);
-            Models.Vertex.CountVertices = 0;
+            Models.Vertex.NumberOfVertex = 0;
             canvas.Add(pentagon);
         }
         /// <summary>
@@ -106,7 +106,7 @@ namespace Shapes.Commands.Pentagon
             {
                 canvas.Add(arrUnsortedVertices[i]);
             }
-            Models.Vertex.CountVertices = Models.Pentagon.NUM_OF_EDGE_IN_PENTAGON - 1;
+            Models.Vertex.NumberOfVertex = Models.Pentagon.NUM_OF_EDGE_IN_PENTAGON - 1;
         }
     }
 }
