c638ddbb606b344c7d8ea175fe6d0b67a9783c30
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-07 11:11:00 +0300

    Update RelayCommand.cs

--- Task2/Project/DataControl/WpfCommands/RelayCommand.cs
+++ Task2/Project/DataControl/WpfCommands/RelayCommand.cs
@@ -1,36 +1,66 @@
-﻿using System;
-using System.Windows.Input;
-
 namespace DataControl.WpfCommands
 {
-    public class RelayCommand : ICommand
+    /// <summary>
+    /// A WPF command which represent action as <see cref="System.Action"/>;
+    /// </summary>
+    public class RelayCommand : System.Windows.Input.ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
+        // FIELDS
+        private System.Action<object> execute;
+        private System.Func<object, bool> canExecute;
 
-        public event EventHandler CanExecuteChanged
+        // EVENT
+        /// <summary>
+        /// Raise when <see cref="DataControl.WpfCommands.RelayCommand.CanExecute(object)"/> changed.
+        /// </summary>
+        public event System.EventHandler CanExecuteChanged
         {
             add
             {
-                CommandManager.RequerySuggested += value;
+                System.Windows.Input.CommandManager.RequerySuggested += value;
             }
             remove
             {
-                CommandManager.RequerySuggested -= value;
+                System.Windows.Input.CommandManager.RequerySuggested -= value;
             }
         }
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        // CONSTRUCTORS
+        /// <summary>
+        /// Initialize a new instance of <see cref="DataControl.WpfCommands.RelayCommand"/>.
+        /// </summary>
+        /// <param name="execute">
+        /// An method that should be executed.
+        /// </param>
+        /// <param name="canExecute">
+        /// An method that checks if command can be executed.
+        /// </param>
+        public RelayCommand(System.Action<object> execute, System.Func<object, bool> canExecute = null)
         {
             this.execute = execute;
             this.canExecute = canExecute;
         }
 
+        // METHODS
+        /// <summary>
+        /// Checks if command can be executed.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter that define if command can be executed.
+        /// </param>
+        /// <returns>
+        /// True — if command can be executed, otherwise — false.
+        /// </returns>
         public bool CanExecute(object parameter)
         {
             return this.canExecute == null || this.canExecute(parameter);
         }
-
+        /// <summary>
+        /// Execute a command.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter which command needs to be executed.
+        /// </param>
         public void Execute(object parameter)
         {
             this.execute(parameter);
