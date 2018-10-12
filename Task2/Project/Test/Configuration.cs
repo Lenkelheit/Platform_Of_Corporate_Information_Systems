52e8edbd3a270e2585eeee057157415cc2d1c69d
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-10-13 00:20:28 +0300

    Update Configuration.cs

--- Task2/Project/Test/Configuration.cs
+++ Task2/Project/Test/Configuration.cs
@@ -5,6 +5,8 @@ namespace Test
         public static readonly string CANVAS_SERIALIZATION_FILE_NAME = @"..\..\Serialization\CanvasData.xml";
         public static readonly string PENTAGON_SERIALIZATION_FILE_NAME = @"..\..\Serialization\PentagonData.xml";
         public static readonly string VERTEX_SERIALIZATION_FILE_NAME = @"..\..\Serialization\VertexData.xml";
+        public static readonly string SHAPEBASE_AND_VERTEX_SERIALIZATION_FILE_NAME = @"..\..\Serialization\ShapeBaseAndVertexData.xml";
+        public static readonly string SHAPEBASE_AND_PENTAGON_SERIALIZATION_FILE_NAME = @"..\..\Serialization\ShapeBaseAndPentagonData.xml";
         public static void UndoAll(Shapes.Models.UndoRedoManager manager)
         {
             while (manager.CanUndo)
