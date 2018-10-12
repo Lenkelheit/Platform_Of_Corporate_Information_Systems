2b01203621cee9f3353b25d8637a16dda2a85df7
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-10-12 23:13:01 +0300

    Update Configuration.cs

--- Task2/Project/Test/Configuration.cs
+++ Task2/Project/Test/Configuration.cs
@@ -4,6 +4,9 @@
     {
         public static readonly string CANVAS_SERIALIZATION_FILE_NAME = @"..\..\Serialization\CanvasData.xml";
         public static readonly string PENTAGON_SERIALIZATION_FILE_NAME = @"..\..\Serialization\PentagonData.xml";
+        
+        public static readonly int POINTS_AMOUNT = 5;
+        
         public static void UndoAll(Shapes.Models.UndoRedoManager manager)
         {
             while (manager.CanUndo)
