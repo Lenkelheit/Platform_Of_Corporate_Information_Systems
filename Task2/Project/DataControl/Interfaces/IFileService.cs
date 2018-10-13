4f10600e024d29cb210aa4c7e7024842329bdef4
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-10-13 18:43:46 +0300

    Update IFileService.cs

--- Task2/Project/DataControl/Interfaces/IFileService.cs
+++ Task2/Project/DataControl/Interfaces/IFileService.cs
@@ -1,8 +1,21 @@
 namespace DataControl.Interfaces
 {
+    /// <summary>
+    /// Represents service for work with files.
+    /// </summary>
     public interface IFileService
     {
+        /// <summary>
+        /// Saves information to file.
+        /// </summary>
+        /// <param name="item">Object to saving.</param>
+        /// <param name="fileName">The name of file.</param>
         void Save(Shapes.Models.Canvas item, string fileName);
+        /// <summary>
+        /// Loads information from file.
+        /// </summary>
+        /// <param name="item">Object to loading.</param>
+        /// <param name="fileName">The name of file.</param>
         void Load(out Shapes.Models.Canvas item, string fileName);
     }
 }
