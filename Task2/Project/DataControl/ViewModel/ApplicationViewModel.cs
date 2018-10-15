0a2bf34f30659229cb31453de71bec4cb3fd720c
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-15 14:15:54 +0300

    Update ApplicationViewModel.cs

--- Task2/Project/DataControl/ViewModel/ApplicationViewModel.cs
+++ Task2/Project/DataControl/ViewModel/ApplicationViewModel.cs
@@ -5,10 +5,14 @@ using DataControl.WpfCommands;
 using Shapes.Models;
 
 using System.ComponentModel;
-
+using System.Windows;
+using System.Windows.Input;
 
 namespace DataControl
 {
+    /// <summary>
+    /// A class that bond view and models.
+    /// </summary>
     public class ApplicationViewModel : INotifyPropertyChanged
     {
         // FIELDS
@@ -65,8 +69,19 @@ namespace DataControl
 
             newFile = new RelayCommand(NewFileMethod);
 
-            throw new System.NotImplementedException();
+            undoAction = new RelayCommand(UndoActionMethod, CanUndoAction);
+            redoAction = new RelayCommand(RedoActionMethod, CanRedoAction);
+            undoManyAction = new RelayCommand(UndoManyItemsMethod);
+            redoManyAction = new RelayCommand(RedoManyActionMethod);
+
+            addVertex = new RelayCommand(AddVertexMethod);
+            deleteShape = new RelayCommand(DeleteShapeMethod, CanDeleteShapeAction);
+
+            manager.PropertyChanged += Manager_PropertyChanged;
+
         }
+
+
 
         // PROPERTIES
         public string FileName
@@ -76,47 +91,64 @@ namespace DataControl
                 throw new System.NotImplementedException();
             }
         }
+        /// <summary>
+        /// Property that enable to interract with selected shape
+        /// </summary>
         public ShapeBase SelectedShape
         {
             get
             {
-                throw new System.NotImplementedException();
+                return selectedShape;
             }
             set
             {
-                throw new System.NotImplementedException();
+                if (value != selectedShape)
+                {
+                    selectedShape = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("SelectedShape"));
+                }
             }
         }
+        /// <summary>
+        /// Property that enable to interract with canvas
+        /// </summary>
         public Canvas Canvas
         {
             get
             {
-                throw new System.NotImplementedException();
-            }
-        }
-        public System.Collections.Generic.IEnumerable<string> shapeNames
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-        public System.Collections.Generic.IEnumerable<string> undoActionNames
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
-        public System.Collections.Generic.IEnumerable<string> redoActionNames
-        {
-            get
-            {
-                throw new System.NotImplementedException();
+                return canvas;
             }
         }
 
-  
+        public System.Collections.Generic.IEnumerable<string> ShapeNames
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+        /// <summary>
+        /// Property that enable to get undo action names
+        /// </summary>
+        public System.Collections.Generic.IEnumerable<string> UndoActionNames
+        {
+            get
+            {
+                return manager.UndoItems;
+            }
+        }
+        /// <summary>
+        /// Property that enable to get redo action names
+        /// </summary>
+        public System.Collections.Generic.IEnumerable<string> RedoActionNames
+        {
+            get
+            {
+                return manager.RedoItems;
+            }
+        }
+
+
 
         public RelayCommand NewFile => newFile;
 
@@ -127,17 +159,29 @@ namespace DataControl
         public RelayCommand SaveAsFile => saveAsFile;
 
         public RelayCommand Exit => exit;
-
+        /// <summary>
+        /// Property that enable to interract with DeleteShape command
+        /// </summary>
         public RelayCommand DeleteShape => deleteShape;
-
+        /// <summary>
+        /// Property that enable to interract with UndoAction command
+        /// </summary>
         public RelayCommand UndoAction => undoAction;
-
+        /// <summary>
+        /// Property that enable to interract with RedoAction command
+        /// </summary>
         public RelayCommand RedoAction => redoAction;
-
+        /// <summary>
+        /// Property that enable to interract with UndoManyAction command
+        /// </summary>
         public RelayCommand UndoManyAction => undoManyAction;
-
+        /// <summary>
+        /// Property that enable to interract with RedoManyAction command
+        /// </summary>
         public RelayCommand RedoManyAction => redoManyAction;
-
+        /// <summary>
+        /// Property that enable to interract with AddVertex command
+        /// </summary>
         public RelayCommand AddVertex => addVertex;
 
         public RelayCommand ChangeShapeColor => changeShapeColor;
@@ -150,11 +194,83 @@ namespace DataControl
 
         public RelayCommand ChangeShapeLocation => changeShapeLocation;
 
+        
+
         // METHODS
         private void NewFileMethod(object o)
         {
             throw new System.NotImplementedException();
         }
-        // your methods here
+        private void AddVertexMethod(object o)
+        {
+            Vertex target = new Vertex()
+            {
+                Location = Mouse.GetPosition((IInputElement)o)
+            };
+            manager.Execute(new Shapes.Commands.Vertex.AddVertex(canvas, target, manager));
+
+            selectedShape = canvas[canvas.Count - 1];
+            OnPropertyChanged(new PropertyChangedEventArgs("Canvas"));
+        }
+        private void DeleteShapeMethod(object o)
+        {
+            if (selectedShape is Pentagon)
+            {
+                manager.Execute(new Shapes.Commands.Pentagon.RemovePentagon(canvas, (Pentagon)selectedShape));
+            }
+            else if (selectedShape is Vertex)
+            {
+                manager.Execute(new Shapes.Commands.Vertex.RemoveVertex(canvas, (Vertex)selectedShape));
+            }
+            else
+            {
+                throw new System.NullReferenceException("Shape don't chosed!");
+            }
+
+            selectedShape = canvas.Count > 0 ? canvas[canvas.Count - 1] : null;
+        }
+        private void UndoActionMethod(object o)
+        {
+            manager.Undo();
+        }
+        private void RedoActionMethod(object o)
+        {
+            manager.Redo();
+        }
+        private void UndoManyItemsMethod(object o)
+        {
+            manager.Undo((int)o + 1);
+        }
+        private void RedoManyActionMethod(object o)
+        {
+            manager.Redo((int)o + 1);
+        }
+
+        // RESTRICTION
+        private bool CanDeleteShapeAction(object o)
+        {
+            return selectedShape != null;
+        }
+        private bool CanUndoAction(object o)
+        {
+            return manager.CanUndo;
+        }
+        private bool CanRedoAction(object o)
+        {
+            return manager.CanRedo;
+        }
+        // EVENT METHODS
+        private void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+        private void Manager_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "UndoItems": OnPropertyChanged(new PropertyChangedEventArgs("UndoActionNames")); break;
+                case "RedoItems": OnPropertyChanged(new PropertyChangedEventArgs("RedoActionNames")); break;
+            }
+        }
     }
 }
