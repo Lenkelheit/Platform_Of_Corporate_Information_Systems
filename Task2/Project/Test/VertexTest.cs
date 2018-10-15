da9c3cd4f0bb18a5efc27b8632d875e5e96b744b
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-15 13:55:09 +0300

    Update VertexTest.cs

--- Task2/Project/Test/VertexTest.cs
+++ Task2/Project/Test/VertexTest.cs
@@ -9,14 +9,23 @@ namespace Test
     public class VertexTest
     {
         [TestMethod]
-        public void ExecuteTest()
+        public void Radius()
         {
+            Vertex v1 = new Vertex();
+            Vertex v2 = new Vertex();
 
-        }
-        [TestMethod]
-        public void UnExecuteTest()
-        {
+            int startRadiusValue = v1.Radius;
 
+            Assert.AreEqual(startRadiusValue, v1.Radius);
+            Assert.AreEqual(startRadiusValue, v2.Radius);
+
+            int newRadiusValue = 8;
+            v1.Radius = newRadiusValue;
+
+            Assert.AreEqual(newRadiusValue, v1.Radius);
+            Assert.AreEqual(newRadiusValue, v2.Radius);
+
+            v1.Radius = startRadiusValue;
         }
     }
 }
