13b120cc532e76be1270336ee45439a896c1b01d
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-28 13:09:04 +0200

    Update ProgressWindow.xaml.cs

--- Task3/Project/Task3/Forms/ProgressWindow.xaml.cs
+++ Task3/Project/Task3/Forms/ProgressWindow.xaml.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace Task3.Forms
 {
     /// <summary>
     /// Interaction logic for ProgressWindow.xaml
     /// </summary>
-    public partial class ProgressWindow : Window
+    public partial class ProgressWindow : System.Windows.Window
     {
         public ProgressWindow()
         {
             InitializeComponent();
+        }
+
+        private void MovingWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Exit(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
         }
     }
 }
