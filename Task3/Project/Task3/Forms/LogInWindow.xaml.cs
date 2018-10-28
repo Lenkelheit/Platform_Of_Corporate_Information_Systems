93d9d88d621e53e3850b751e85ccd5c346c237cc
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-28 13:06:27 +0200

    Update LogInWindow.xaml.cs

--- Task3/Project/Task3/Forms/LogInWindow.xaml.cs
+++ Task3/Project/Task3/Forms/LogInWindow.xaml.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace Task3.Forms
 {
     /// <summary>
     /// Interaction logic for LogInWindow.xaml
     /// </summary>
-    public partial class LogInWindow : Window
+    public partial class LogInWindow : System.Windows.Window
     {
         public LogInWindow()
         {
             InitializeComponent();
+        }
+
+        private void MovingWindow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private void Exit(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
         }
     }
 }
