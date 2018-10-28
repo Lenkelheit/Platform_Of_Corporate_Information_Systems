95e9ca8fbda456fb04e9d4c37f7600a7cb04a67a
Kizlo Taras <orleon.nilkford@gmail.com>
2018-10-28 13:07:27 +0200

    Update MainWindow.xaml.cs

--- Task3/Project/Task3/Forms/MainWindow.xaml.cs
+++ Task3/Project/Task3/Forms/MainWindow.xaml.cs
@@ -1,46 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DataControl.Services;
+using DataControl.ViewModel;
 
 namespace Task3
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Forms.CabinetWindow cabinet = new Forms.CabinetWindow();
-            cabinet.ShowDialog();
-
-            Forms.LogInWindow logIn = new Forms.LogInWindow();
-            logIn.ShowDialog();
-
-            Forms.MessageBoxWindow messageBox = new Forms.MessageBoxWindow();
-            messageBox.ShowDialog();
-
-            Forms.ProgressWindow progress = new Forms.ProgressWindow();
-            progress.ShowDialog();
-
-            Forms.ScoreWindow score = new Forms.ScoreWindow();
-            score.ShowDialog();
+            /*this.DataContext = new ApplicationViewModel(
+                new CsvFileService()
+                    .SetConfiguration(new FileConfiguration()
+                    {
+                        ClientFile = "/Resources/files/Client.csv",
+                        DriverFile = "/Resources/files/Driver.csv",
+                        RouteFile = "/Resources/files/Route.csv",
+                        ScoreFile = "/Resources/files/Score.csv",
+                        StreetFile = "/Resources/files/Street.csv",
+                    }));*/
         }
     }
 }
