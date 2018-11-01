51d231a85f514ae05339336d4c8cd0dd1556d2ed
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-01 09:36:29 +0200

    Update Driver.cs

--- Task3/Project/TaxiDriver/Classes/Driver.cs
+++ Task3/Project/TaxiDriver/Classes/Driver.cs
@@ -1,45 +1,91 @@
-﻿namespace TaxiDriver
+namespace TaxiDriver
 {
+    /// <summary>
+    /// Represents class that models Driver.
+    /// </summary>
     public class Driver
     {
+        // FIELDS
+        private string name;
+        private string password;
+        private double bestScore;
+        private double totalScore;
+        private double lastScore;
+        // PROPERTIES
+        /// <summary>
+        /// Property that defines the name.
+        /// </summary>
         public string Name
         {
             get
             {
-                throw new System.NotImplementedException();
+                return name;
             }
         }
+        /// <summary>
+        /// Property that defines the password.
+        /// </summary>
         public string Password
         {
             get
             {
-                throw new System.NotImplementedException();
+                return password;
             }
         }
+        /// <summary>
+        /// Property that defines the name best score.
+        /// </summary>
         public double BestScore
         {
             get
             {
-                throw new System.NotImplementedException();
+                return bestScore;
             }
         }
+        /// <summary>
+        /// Property that defines the total score.
+        /// </summary>
         public double TotalScore
         {
             get
             {
-                throw new System.NotImplementedException();
+                return totalScore;
             }
         }
+        /// <summary>
+        /// Property that defines the last score.
+        /// </summary>
         public double LastScore
         {
             get
             {
-                throw new System.NotImplementedException();
+                return lastScore;
             }
             set
             {
-                throw new System.NotImplementedException();
+                if(value < 0)
+                {
+                    throw new System.ArgumentOutOfRangeException("score can not be zero");
+                }
+                lastScore = value;
             }
+        }
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="password"></param>
+        /// <param name="bestScore"></param>
+        /// <param name="totalScore"></param>
+        /// <param name="lastScore"></param>
+        public Driver(string name, string password, double bestScore, double totalScore, double lastScore)
+        {
+            this.name = name;
+            this.password = password;
+            this.bestScore = bestScore;
+            this.totalScore = totalScore;
+            this.lastScore = lastScore;
         }
     }
 }
