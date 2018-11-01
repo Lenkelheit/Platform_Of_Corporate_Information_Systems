a4bef0b2d959a594d7aeb8d392cbfd81453e1c7f
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-01 09:37:58 +0200

    Update Route.cs

--- Task3/Project/TaxiDriver/Struct/Route.cs
+++ Task3/Project/TaxiDriver/Struct/Route.cs
@@ -1,34 +1,70 @@
-﻿namespace TaxiDriver
+namespace TaxiDriver
 {
+    /// <summary>
+    /// Represents class that models Route.
+    /// </summary>
     public struct Route
     {
+        // FIELDS
+        private string startStreet;
+        private string endStreet;
+        private System.TimeSpan time;
+        private double price;
+        // PROPERTIES
+        /// <summary>
+        /// Property that defines the start street.
+        /// </summary>
         public string StartStreet
         {
             get
             {
-                throw new System.NotImplementedException();
+                return startStreet;
             }
         }
+        /// <summary>
+        /// Property that defines the end street.
+        /// </summary>
         public string EndStreet
         {
             get
             {
-                throw new System.NotImplementedException();
+                return endStreet;
             }
         }
+        /// <summary>
+        /// Property that defines the time.
+        /// </summary>
         public System.TimeSpan Time
         {
             get
             {
-                throw new System.NotImplementedException();
+                return time;
             }
         }
+        /// <summary>
+        /// Property that defines the price.
+        /// </summary>
         public double Price
         {
             get
             {
-                throw new System.NotImplementedException();
+                return price;
             }
+        }
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="startStreet"></param>
+        /// <param name="endStreet"></param>
+        /// <param name="time"></param>
+        /// <param name="price"></param>
+        public Route(string startStreet, string endStreet, System.TimeSpan time, double price)
+        {
+            this.startStreet = startStreet;
+            this.endStreet = endStreet;
+            this.time = time;
+            this.price = price;
         }
     }
 }
