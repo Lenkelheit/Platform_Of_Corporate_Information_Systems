833c82364ea562b51d96767cb619288f7e893c95
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-01 09:37:37 +0200

    Update Client.cs

--- Task3/Project/TaxiDriver/Struct/Client.cs
+++ Task3/Project/TaxiDriver/Struct/Client.cs
@@ -1,20 +1,44 @@
-﻿namespace TaxiDriver
+namespace TaxiDriver
 {
+    /// <summary>
+    /// Represents class that models Client.
+    /// </summary>
     public struct Client
     {
+        // FILEDS
+        private string name;
+        private string phone;
+        // PROPERTIES
+        /// <summary>
+        /// Property that defines the name.
+        /// </summary>
         public string Name
         {
             get
             {
-                throw new System.NotImplementedException();
+                return name;
             }
         }
+        /// <summary>
+        /// Property that defines the phone.
+        /// </summary>
         public string Phone
         {
             get
             {
-                throw new System.NotImplementedException();
+                return phone;
             }
+        }
+        // CONSTRUCTORS
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="phone"></param>
+        public Client(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
         }
     }
 }
