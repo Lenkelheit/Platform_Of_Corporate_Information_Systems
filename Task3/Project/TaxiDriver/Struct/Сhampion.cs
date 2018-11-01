850e93e8bb38d4cf82f8b050bfdbe7fb2e9d9d58
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-01 09:38:25 +0200

    Update Сhampion.cs

--- Task3/Project/TaxiDriver/Struct/Сhampion.cs
+++ Task3/Project/TaxiDriver/Struct/Сhampion.cs
@@ -1,27 +1,57 @@
-﻿namespace TaxiDriver
+namespace TaxiDriver
 {
+    /// <summary>
+    /// Represents class that models Champion.
+    /// </summary>
     public struct Сhampion
     {
+        // FIELDS
+        private int number;
+        private string name;
+        private double score;
+        // PROPERTIES
+        /// <summary>
+        /// Property that defines the number.
+        /// </summary>
         public int Number
         {
             get
             {
-                throw new System.NotImplementedException();
+                return number;
             }
         }
+        /// <summary>
+        /// Property that defines the name.
+        /// </summary>
         public string Name
         {
             get
             {
-                throw new System.NotImplementedException();
+                return name;
             }
         }
+        /// <summary>
+        /// Property that defines the score.
+        /// </summary>
         public double Score
         {
             get
             {
-                throw new System.NotImplementedException();
+                return score;
             }
+        }
+        // CONSTROCTORS
+        /// <summary>
+        /// Constructors with parameters.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="name"></param>
+        /// <param name="score"></param>
+        public Сhampion(int number, string name, double score)
+        {
+            this.number = number;
+            this.name = name;
+            this.score = score;
         }
     }
 }
