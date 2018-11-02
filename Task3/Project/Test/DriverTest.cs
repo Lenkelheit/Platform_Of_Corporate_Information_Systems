3f161d5d0e26d93f7771dd2216abb112ae1f02e3
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-02 22:29:42 +0200

    Update DriverTest.cs

--- Task3/Project/Test/DriverTest.cs
+++ Task3/Project/Test/DriverTest.cs
@@ -1,14 +1,61 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxiDriver;
 
 namespace Test
 {
     [TestClass]
     public class DriverTest
-    {
+    {       
         [TestMethod]
-        public void TestMethod1()
+        public void NameTest()
         {
+            string expectedName = "Jason";
+
+            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Assert.IsTrue(driver.Name == expectedName);
+        }
+
+        [TestMethod]
+        public void PasswordTest()
+        {
+            string expectedPasswword = "pa$$word";
+
+            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Assert.IsTrue(driver.Password == expectedPasswword);
+        }
+
+        [TestMethod]
+        public void BestScoreTest()
+        {
+            double expectedBestSore = 100;
+
+            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Assert.IsTrue(driver.BestScore == expectedBestSore);
+        }
+
+        [TestMethod]
+        public void TotalScoreTest()
+        {
+            double expectedTotalSore = 76;
+
+            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Assert.IsTrue(driver.TotalScore == expectedTotalSore);
+        }
+
+        [TestMethod]
+        public void ConstrucrtorTest()
+        {
+            string expectedName = "Jason";
+            string expectePassword = "pa$$word";
+            double expectedBestScore = 100;
+            double expectedTotalScore = 76;
+
+            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Assert.IsTrue(driver.Name == expectedName 
+                       && driver.Password == expectePassword
+                       && driver.BestScore == expectedBestScore 
+                       && driver.TotalScore == expectedTotalScore);
         }
     }
 }
