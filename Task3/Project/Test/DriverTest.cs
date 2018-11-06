7087f30e0bf489b2477dc0c3a6b5923d88236e26
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-06 22:36:10 +0200

    Update DriverTest.cs

--- Task3/Project/Test/DriverTest.cs
+++ Task3/Project/Test/DriverTest.cs
@@ -12,7 +12,7 @@ namespace Test
         {
             string expectedName = "Jason";
 
-            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Driver driver = new Driver("Jason", "pa$$word", 76, 100);
             Assert.IsTrue(driver.Name == expectedName);
         }
 
@@ -21,35 +21,39 @@ namespace Test
         {
             string expectedPasswword = "pa$$word";
 
-            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Driver driver = new Driver("Jason", "pa$$word", 76, 100);
             Assert.IsTrue(driver.Password == expectedPasswword);
         }
 
         [TestMethod]
         public void BestScoreTest()
         {
-            double expectedBestSore = 100;
+            double expectedBestSore = 76;
 
-            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Driver driver = new Driver("Jason", "pa$$word", 76, 100);
             Assert.IsTrue(driver.BestScore == expectedBestSore);
         }
 
         [TestMethod]
         public void TotalScoreTest()
         {
-            double expectedTotalSore = 76;
-
-            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            double expectedTotalSore = 100;
+            Driver driver = new Driver("Jason", "pa$$word", 76, 100);
             Assert.IsTrue(driver.TotalScore == expectedTotalSore);
         }
-            
+
         [TestMethod]
         public void LastScore()
         {
-            double expectedLastScore = 0;
+            double expectedLastScore = 80;
+            double expectedBestScore = 80;
+            double expectedTotalScore = 180;
 
-            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
-            Assert.IsTrue(driver.LastScore == expectedLastScore);           
+            Driver driver = new Driver("Jason", "pa$$word", 76, 100);
+            driver.LastScore = 80;
+            Assert.IsTrue(driver.BestScore == expectedBestScore
+                       && driver.TotalScore == expectedTotalScore
+                       && driver.LastScore == expectedLastScore);           
         }
 
         [TestMethod]
@@ -57,11 +61,11 @@ namespace Test
         {
             string expectedName = "Jason";
             string expectePassword = "pa$$word";
-            double expectedBestScore = 100;
-            double expectedTotalScore = 76;
+            double expectedBestScore = 76;
+            double expectedTotalScore = 100;
             double expectedLastScore = 0;
 
-            Driver driver = new Driver("Jason", "pa$$word", 100, 76);
+            Driver driver = new Driver("Jason", "pa$$word", 76, 100);
             Assert.IsTrue(driver.Name == expectedName 
                        && driver.Password == expectePassword
                        && driver.BestScore == expectedBestScore 
