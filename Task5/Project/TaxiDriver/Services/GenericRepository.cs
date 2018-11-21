d2ec86339fe3653616ed21edd38b188599d891cf
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-11-21 21:15:44 +0200

    Driver context and GenericRepository update

--- Task5/Project/TaxiDriver/Services/GenericRepository.cs
+++ Task5/Project/TaxiDriver/Services/GenericRepository.cs
@@ -6,20 +6,39 @@ using System.Collections.Generic;
 
 namespace TaxiDriver.Services
 {
+    /// <summary>
+    /// Represents class that proxy behind data acsess and buisness logic
+    /// </summary>
+    /// <typeparam name="TEntity">Data class work with</typeparam>
     public class GenericRepository<TEntity> : Interfaces.IGenericRepository<TEntity> where TEntity : class
     {
         // FIELDS
         internal Context.DriverContext context;
         internal DbSet<TEntity> dbSet;
         // CONSTRUCTORS
+        /// <summary>
+        /// Basic constructor with 1 parameter
+        /// </summary>
+        /// <param name="context">Data context</param>
         public GenericRepository(Context.DriverContext context)
         {
             this.context = context;
             this.dbSet = context.Set<TEntity>();
         }
         // PROPERTIES
+        /// <summary>
+        /// Property that enale to interruct with count of entities in data base
+        /// </summary>
+        /// <returns>Count of entities</returns>
         public int Count => dbSet.Count();
         // METHODS
+        /// <summary>
+        /// Method that data from data base
+        /// </summary>
+        /// <param name="filter">Filter for data</param>
+        /// <param name="orderBy">The order of the received items</param>
+        /// <param name="includeProperties">Included properties</param>
+        /// <returns>Queried entities collection</returns>
         public virtual IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>,
                                                 IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
         {
@@ -43,18 +62,35 @@ namespace TaxiDriver.Services
                 return query.ToList();
             }
         }
+        /// <summary>
+        /// Method that enable to get entity by id
+        /// </summary>
+        /// <param name="id">Entities id</param>
+        /// <returns>Finded entity</returns>
         public virtual TEntity GetByID(object id)
         {
             return dbSet.Find(id);
         }
+        /// <summary>
+        /// Inserts data in data base
+        /// </summary>
+        /// <param name="entity">Inserted entity</param>
         public virtual void Insert(TEntity entity)
         {
             dbSet.Add(entity);
         }
+        /// <summary>
+        /// Deletes object by id
+        /// </summary>
+        /// <param name="id">Objects id</param>
         public virtual void Delete(object id)
         {
             Delete(dbSet.Find(id));
         }
+        /// <summary>
+        /// Deletes preset entity
+        /// </summary>
+        /// <param name="entityToDelete">Entity to delete</param>
         public virtual void Delete(TEntity entityToDelete)
         {
             if (context.Entry(entityToDelete).State == EntityState.Detached)
@@ -63,6 +99,10 @@ namespace TaxiDriver.Services
             }
             dbSet.Remove(entityToDelete);
         }
+        /// <summary>
+        /// Updates data base
+        /// </summary>
+        /// <param name="entityToUpdate">Entity to update</param>
         public virtual void Update(TEntity entityToUpdate)
         {
             dbSet.Attach(entityToUpdate);
