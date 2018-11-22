9f1cc006def52ef43989df3fa0889306e3868a9d
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-11-22 18:46:24 +0200

    UnitOfWork update

--- Task5/Project/TaxiDriver/Services/UnitOfWork.cs
+++ Task5/Project/TaxiDriver/Services/UnitOfWork.cs
@@ -3,71 +3,126 @@ using TaxiDriver.Interfaces;
 
 namespace TaxiDriver.Services
 {
+    /// <summary>
+    /// Repsresents class that enable to work with entities repository and gives away context for it
+    /// </summary>
     public class UnitOfWork : Interfaces.IUnitOfWork
     {
         // FIELDS
+        GenericRepository<Client> clientRepository;
+        GenericRepository<DriverInfo> driverInfoRepository;
+        GenericRepository<Route> routeRepository;
+        GenericRepository<Score> scoreRepository;
+        GenericRepository<Street> streetRepository;
+        Context.DriverContext db;
+
         private bool disposedValue = false; // To detect redundant calls
         // CONSTRUCTORS
-         // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~UnitOfWork() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
+        /// <summary>
+        /// Basic costructor without parameters
+        /// </summary>
+        public UnitOfWork()
+        {
+            db = new Context.DriverContext();
+            clientRepository = new GenericRepository<Client>(db);
+            driverInfoRepository = new GenericRepository<DriverInfo>(db);
+            routeRepository = new GenericRepository<Route>(db);
+            scoreRepository = new GenericRepository<Score>(db);
+            streetRepository = new GenericRepository<Street>(db);
+        }
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        /// <summary>
+        /// Default finaliser
+        /// </summary>
+        ~UnitOfWork()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
         // PROPERTIES
+        /// <summary>
+        /// Property that enable to interruct with client repository
+        /// </summary>
+        /// <returns>Client Repository</returns>
         public IGenericRepository<Client> ClientRepository
         {
             get
             {
-                throw new NotImplementedException();
+                return clientRepository;
             }
         }
-
+        /// <summary>
+        /// Property that enable to interruct with driver info repository
+        /// </summary>
+        /// <returns>Driver info Repository</returns>
         public IGenericRepository<DriverInfo> DriverInfoRepository
         {
             get
             {
-                throw new NotImplementedException();
+                return driverInfoRepository;
             }
         }
-
+        /// <summary>
+        /// Property that enable to interruct with route repository
+        /// </summary>
+        /// <returns>Route Repository</returns>
         public IGenericRepository<Route> RouteRepository
         {
             get
             {
-                throw new NotImplementedException();
+                return routeRepository;
             }
         }
-
+        /// <summary>
+        /// Property that enable to interruct with score repository
+        /// </summary>
+        /// <returns>Score Repository</returns>
         public IGenericRepository<Score> ScoreRepository
         {
             get
             {
-                throw new NotImplementedException();
+                return scoreRepository;
             }
         }
-
+        /// <summary>
+        /// Property that enable to interruct with street repository
+        /// </summary>
+        /// <returns>Street Repository</returns>
         public IGenericRepository<Street> StreetRepository
         {
             get
             {
-                throw new NotImplementedException();
+                return streetRepository;
             }
         }
 
         // METHODS
         // This code added to correctly implement the disposable pattern.
+        /// <summary>
+        /// Dispose resourses
+        /// </summary>
         public void Dispose()
         {
             // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
             // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+            GC.SuppressFinalize(this);
         }
-
+        /// <summary>
+        /// Save changes in data base
+        /// </summary>
         public void Save()
         {
-            throw new NotImplementedException();
+            clientRepository.context.SaveChanges();
+            driverInfoRepository.context.SaveChanges();
+            routeRepository.context.SaveChanges();
+            scoreRepository.context.SaveChanges();
+            streetRepository.context.SaveChanges();
         }
+        /// <summary>
+        /// Special disposer
+        /// </summary>
+        /// <param name="disposing">Dispose managed resourses</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
@@ -75,13 +130,19 @@ namespace TaxiDriver.Services
                 if (disposing)
                 {
                     // TODO: dispose managed state (managed objects).
+                    clientRepository = null;
+                    driverInfoRepository = null;
+                    routeRepository = null;
+                    scoreRepository = null;
+                    streetRepository = null;
                 }
-
                 // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                db.Dispose();
+                db = null;
                 // TODO: set large fields to null.
 
                 disposedValue = true;
             }
-        }    
+        }
     }
 }
