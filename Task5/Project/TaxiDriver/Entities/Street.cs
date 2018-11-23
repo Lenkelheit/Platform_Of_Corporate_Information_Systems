7e16721050fe73626e9e95b4691e5e755a2add9f
Василь Танчинець <vasyltanchinets98@gmail.com>
2018-11-23 23:13:03 +0200

    Update Street.cs

--- Task5/Project/TaxiDriver/Entities/Street.cs
+++ Task5/Project/TaxiDriver/Entities/Street.cs
@@ -1,8 +1,17 @@
-﻿namespace TaxiDriver
+namespace TaxiDriver
 {
+    /// <summary>
+    /// Represents class that entities Street
+    /// </summary>
     public class Street
     {
+        /// <summary>
+        /// Property that defines the identifier
+        /// </summary>
         public int ID { get; set; }
+        /// <summary>
+        /// Property that defines the name
+        /// </summary>
         public string Name { get; set; }
     }
 }
