3b34eaa53abedeb44b8097efd7c16c8c8a475162
Nazariy Tymtsiv <38116562+Lenkelheit@users.noreply.github.com>
2018-11-25 02:50:18 +0200

    Update DataBaseService.cs

--- Task5/Project/DataControl/Services/DataBaseService.cs
+++ Task5/Project/DataControl/Services/DataBaseService.cs
@@ -1,57 +1,280 @@
-﻿using System;
-using DataControl.Interfaces;
 using TaxiDriver;
+using System.Linq;
 
 namespace DataControl.Services
 {
+    /// <summary>
+    /// Represents service for work with databases.
+    /// </summary>
     public class DataBaseService : Interfaces.IDataAccessService
     {
-        DBConfiguration dbConfiguration;
+        // FIELDS
+        private Driver driver;
+        private string message;
+        private DBConfiguration dbConfiguration;
+        private System.Random rand;
 
+        // PROPERTIES
+        /// <summary>
+        /// Current driver.
+        /// </summary>
         public Driver Driver
         {
             get
             {
-                throw new NotImplementedException();
+                return driver;
             }
         }
-
+        /// <summary>
+        /// Some special information about exceptional service state.
+        /// </summary>
         public string Message
         {
             get
             {
-                throw new NotImplementedException();
+                return message;
             }
         }
 
+        // CONSTRUCTORS
+        /// <summary>
+        /// Basic constructors without parameters.
+        /// </summary>
+        public DataBaseService()
+        {
+            driver = null;
+            message = null;
+            dbConfiguration = null;
+            rand = new System.Random();
+        }
+
+        // METHODS
+        private void SetNullToDriverAndMessage()
+        {
+            driver = null;
+            message = null;
+        }
+
+        private void CheckDatabaseConfiguration()
+        {
+            if (dbConfiguration == null)
+            {
+                throw new System.ArgumentNullException("The configuration isn`t setted up.");
+            }
+        }
+
+        private Client CreateClient()
+        {
+            return dbConfiguration.UnitOfWork.ClientRepository
+                .GetByID(rand.Next(1, dbConfiguration.UnitOfWork.ClientRepository.Count + 1));
+        }
+
+        private Route CreateRoute()
+        {
+            return dbConfiguration.UnitOfWork.RouteRepository
+                .GetByID(rand.Next(1, dbConfiguration.UnitOfWork.RouteRepository.Count + 1));
+        }
+
+        /// <summary>
+        /// Gets the best champions(drivers) from some database.
+        /// </summary>
+        /// <param name="amount">Amount of champions for choosing from beginning of some database.</param>
+        /// <returns>
+        /// Array of champions.
+        /// </returns>
+        /// <exception cref="System.Data.DataException">
+        /// Thrown when database wasn`t found.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when database configuration is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <see cref="DBConfiguration.UnitOfWork"/> is null.
+        /// </exception>
         public Champion[] GetBest(int amount)
         {
-            throw new NotImplementedException();
+            CheckDatabaseConfiguration();
+
+            Score[] bestScores = dbConfiguration.UnitOfWork.ScoreRepository
+                .Get(orderBy: query => query.GroupBy(scoreElem => scoreElem.DriverInfoID)
+                .Select(group => group.OrderByDescending(scoreElemInGroup => scoreElemInGroup.Scores).First())
+                .OrderByDescending(uniqueDriverScore => uniqueDriverScore.Scores)).Take(amount).ToArray();
+
+            Champion[] champions = new Champion[bestScores.Length];
+
+            DriverInfo driverInfo = null;
+            for (int i = 0; i < bestScores.Length; ++i) 
+            {
+                driverInfo = dbConfiguration.UnitOfWork.DriverInfoRepository.GetByID(bestScores[i].DriverInfoID);
+                champions[i] = new Champion(i + 1, driverInfo.Name, bestScores[i].Scores);
+            }
+            return champions;
         }
 
+        /// <summary>
+        /// Gets the random order from some database.
+        /// </summary>
+        /// <returns>
+        /// The random order.
+        /// </returns>
+        /// <exception cref="System.Data.DataException">
+        /// Thrown when database wasn`t found.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when database configuration is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <see cref="DBConfiguration.UnitOfWork"/> is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when the clients table of the database is empty.
+        /// </exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when the routes table of the database is empty.
+        /// </exception>
         public Order GetRandomOrder()
         {
-            throw new NotImplementedException();
+            CheckDatabaseConfiguration();
+
+            return new Order(rand.Next(1000), CreateClient(), CreateRoute());
         }
 
-        public bool LogIn(string name, string password)
+        /// <summary>
+        /// Sets database configuration.
+        /// </summary>
+        /// <param name="configuration">Some configuration for setting up.</param>
+        /// <returns>
+        /// Current service for data access.
+        /// </returns>
+        /// <exception cref="System.ArgumentException">
+        /// Thrown when the configuration is inappropriate.
+        /// </exception>
+        public Interfaces.IDataAccessService SetConfiguration(Interfaces.IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            if (!(configuration is DBConfiguration)) 
+            {
+                throw new System.ArgumentException("The configuration is inappropriate.");
+            }
+            dbConfiguration = (DBConfiguration)configuration;
+            return this;
         }
 
-        public void SaveResult(Driver driver)
+        /// <summary>
+        /// Saves driver result to some database.
+        /// </summary>
+        /// <param name="currentDriver">Current driver.</param>
+        /// <exception cref="System.Data.DataException">
+        /// Thrown when database with <see cref="DriverInfo"/> wasn`t found.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when database configuration is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <see cref="DBConfiguration.UnitOfWork"/> is null.
+        /// </exception>
+        public void SaveResult(Driver currentDriver)
         {
-            throw new NotImplementedException();
+            CheckDatabaseConfiguration();
+
+            DriverInfo driverInformation =
+                dbConfiguration.UnitOfWork.DriverInfoRepository
+                .Get(filter: driverInfo => driverInfo.Name == currentDriver.Name).First();
+            driverInformation.Scores.Add(new Score { Scores = currentDriver.LastScore });
+
+            dbConfiguration.UnitOfWork.DriverInfoRepository.Update(driverInformation);
+            dbConfiguration.UnitOfWork.Save();
         }
 
-        public IDataAccessService SetConfiguration(IConfiguration configuration)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Signs up driver by his name and password.
+        /// </summary>
+        /// <param name="name">Driver name.</param>
+        /// <param name="password">Driver password.</param>
+        /// <returns>
+        /// True if driver was signed up, else - false.
+        /// </returns>
+        /// <exception cref="System.Data.DataException">
+        /// Thrown when database with <see cref="DriverInfo"/> wasn`t found.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when database configuration is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <see cref="DBConfiguration.UnitOfWork"/> is null.
+        /// </exception>
         public bool SignUp(string name, string password)
         {
-            throw new NotImplementedException();
+            CheckDatabaseConfiguration();
+
+            SetNullToDriverAndMessage();
+
+            name = name.Trim();
+            password = password.Trim();
+
+            int driversAmount = dbConfiguration.UnitOfWork.DriverInfoRepository
+                .Get(filter: driverInfo => driverInfo.Name == name).Count();
+
+            if (driversAmount != 0)
+            {
+                message = "User with such name already exists.";
+                return false;
+            }
+            driver = new Driver(name, password, 0, 0);
+
+            dbConfiguration.UnitOfWork.DriverInfoRepository.Insert(new DriverInfo
+            {
+                Name = name,
+                Password = password,
+                Scores = new System.Collections.Generic.List<Score>()
+            });
+            dbConfiguration.UnitOfWork.Save();
+            return true;
+        }
+
+        /// <summary>
+        /// Logs in driver by his name and password.
+        /// </summary>
+        /// <param name="name">Driver name.</param>
+        /// <param name="password">Driver password.</param>
+        /// <returns>
+        /// True if driver was logged in, else - false.
+        /// </returns>
+        /// <exception cref="System.Data.DataException">
+        /// Thrown when database with <see cref="DriverInfo"/> wasn`t found.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when database configuration is null.
+        /// </exception>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <see cref="DBConfiguration.UnitOfWork"/> is null.
+        /// </exception>
+        public bool LogIn(string name, string password)
+        {
+            CheckDatabaseConfiguration();
+
+            SetNullToDriverAndMessage();
+
+            name = name.Trim();
+            password = password.Trim();
+
+            var drivers = dbConfiguration.UnitOfWork.DriverInfoRepository
+                .Get(filter: driverInfo => driverInfo.Name == name);
+
+            if (drivers.Count() == 0)  
+            {
+                message = "The name is incorrect.";
+                return false;
+            }
+
+            DriverInfo currentDriver = drivers.First();
+            if (currentDriver.Password != password) 
+            {
+                message = "The password is incorrect.";
+                return false;
+            }
+            driver = new Driver(currentDriver);
+
+            return true;
         }
     }
 }
